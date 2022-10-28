6895c23e6e6c61e1779c84347778dc897c1cee6c
N.Fujita <naofuji.1220@gmail.com>
2022-10-28 20:21:40 +0900

    modify

--- execute_lab.sh
+++ execute_lab.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 #PBS -M naofuji.1220@gmail.com
 #PBS -m be
 #PBS -l nodes=1:ppn=20
 
 cd $PBS_O_WORKDIR
 
-source /opt/intel/bin/compilervars.sh -arch intel64 -platform linux
-PATH=/opt/openmpi/bin:$PATH
-PATH=/opt/intel/compilers_and_libraries/linux/bin/intel64:$PATH
-LD_LIBRARY_PATH=/opt/openmpi/lib:$LD_LIBRARY_PATH
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/compilers_and_libraries/linux/lib/intel64
-MANPATH=/opt/openmpi/share/man:$MANPATH
-export PATH LD_LIBRARY_PATH MANPATH
-
 # 20プロセス並列
 python3 generate.py
-mpirun -np 20 /home/Fujita388/github/lammps/src/lmp_mpi < decomp.input
+mpirun -np 20 /home/Fujita388/github/lammps/src/lmp_mpi < decomp.input > decomp.out
 python3 rescale.py > rescale.atoms
-mpirun -np 20 /home/Fujita388/github/lammps/src/lmp_mpi < rescale.input
+mpirun -np 20 /home/Fujita388/github/lammps/src/lmp_mpi < rescale.input > rescale.out
--- execute_ohtaka.sh
+++ execute_ohtaka.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 4
 #SBATCH -n 128
 #SBATCH -c 1
-#SBATCH -t 00:20:00
+#SBATCH -t 00:40:00
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=naofuji.1220@gmail.com
@@ -12,6 +12,6 @@
 source /home/issp/materiapps/intel/lammps/lammpsvars.sh
 
 python3 generate.py
-srun lammps < decomp.input
+srun lammps < decomp.input > decomp.out
 python3 rescale.py > rescale.atoms
-srun lammps < rescale.input
+srun lammps < rescale.input > rescale.out
